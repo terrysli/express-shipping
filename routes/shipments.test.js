e00abd6c00a61261a18aeb896f3e844f4f508328
Terry Li <terryseanli@gmail.com>
2023-04-11 16:31:16 -0700

    added tests for post /shipments route

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -22,4 +22,34 @@ describe("POST /", function () {
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+  test("throws error if productId is not >= 1000", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        productId: 1,
+        name: "Test Tester",
+        addr: "100 Test St",
+        zip: "12345-6789",
+      });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message[0]).
+      toEqual("instance.productId must be greater than or equal to 1000")
+  });
+
+  test("throws error if zip is not a string", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        productId: 1000,
+        name: "Test Tester",
+        addr: "100 Test St",
+        zip: 12345-6789,
+      });
+
+    expect(resp.body.error.message[0]).
+      toEqual("instance.zip is not of a type(s) string");
+    expect(resp.statusCode).toEqual(400);
+  });
 });
