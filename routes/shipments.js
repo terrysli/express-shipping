5fe04b7f5f68a299f5a63a4faee5fe7cff4be037
Terry Li <terryseanli@gmail.com>
2023-04-11 16:16:47 -0700

    first commit + added JSON schema and validators for /shipments route

---
+++ routes/shipments.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express = require("express");
+const { BadRequestError } = require("../expressError");
+const router = new express.Router();
+const jsonschema = require("jsonschema");
+const shipSchema = require("../schemas/shipSchema.json");
+
+const { shipProduct } = require("../shipItApi");
+
+/** POST /ship
+ *
+ * VShips an order coming from json body:
+ *   { productId, name, addr, zip }
+ *
+ * Returns { shipped: shipId }
+ */
+
+router.post("/", async function (req, res, next) {
+  const result = jsonschema.validate(
+    req.body, shipSchema, {required: true});
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const { productId, name, addr, zip } = req.body;
+  const shipId = await shipProduct({ productId, name, addr, zip });
+  return res.json({ shipped: shipId });
+});
+
+
+module.exports = router;
